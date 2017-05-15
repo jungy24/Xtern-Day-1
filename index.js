4446dd8e1e912ea4b10f61c8ea0d2b64c64e847d
Your Full Name <jungy24@gmail.com>
2017-05-15 11:20:13 -0400

    Fix: add person name to details

---
+++ index.js
@@ -0,0 +1,12 @@
+const personForm = document.querySelector('form')
+
+personForm.onsubmit = (ev) => {
+    ev.preventDefault()
+    const form = ev.target
+    const details = document.querySelector('.details')
+
+    const personName = form.personName.value
+
+    details.textContent = personName
+    
+}

8557573b078ad11c35df91422881b6edfbd9b19c
Your Full Name <jungy24@gmail.com>
2017-05-15 15:32:28 -0400

    Completed hw

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
+
 const personForm = document.querySelector('form')
 
-personForm.onsubmit = (ev) => {
-    ev.preventDefault()
-    const form = ev.target
-    const details = document.querySelector('.details')
+const handleSubmit = (ev) => {
+  ev.preventDefault()
+  const form = ev.target
+  const details = document.querySelector('.details')
 
-    const personName = form.personName.value
+  const personName = form.personName.value
+  const hairColor = form.hairColor.value
+  const age = form.age.value
+  const birthplace = form.birthplace.value
 
-    // details.textContent = `<em>${personName}</em>`
+  //const colorDiv = `
+  //  <div style="height: 50px; width: 100px; background-color: ${hairColor}"></div>
+  //`
 
-    const em = document.createElement('em')
-    em.textContent = personName
+//   details.innerHTML = `
+//     <ul>
+//       <li>Name: ${personName}</li>
+//       <li>Hair Color: ${colorDiv}</li>
+//       <li>Age: ${age}</li>
+//       <li>Birthplace: ${birthplace}</li>
+//     </ul>
+//   `
+
+    const ul = document.createElement('ul')
+
+    const em = document.createElement('li')
+    em.textContent = 'Name: ' + personName 
     details.appendChild(em)
+    //em.textContent = 'Hair Color: ' + colorDiv
+    const am = document.createElement('li')
+     am.textContent = 'Hair Color'
+    // am.style.background = hairColor
 
+    am.style.width = '100px'
+    am.style.height = '50px'
+    am.style.backgroundColor = hairColor
+    details.appendChild(am)
+    const am2 = document.createElement('li')
+    am2.textContent = 'Age: ' + age
+    details.appendChild(am2)
+    const am3 = document.createElement('li')
+    am3.textContent = 'Birthplace: ' + birthplace
+    details.appendChild(am3)
+
+    
 }
+
+personForm.addEventListener('submit', handleSubmit)
